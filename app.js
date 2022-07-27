1373540f334603502e8aae6f71a0fe756a8f6d5d
amir-kedis <amir.kedis@gmail.com>
2022-07-27 19:00:46 +0200

    add the makeGrid function
    - make grid
    - delete canvas
    - attach them to range

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const rangeInput = document.querySelector("#rangeInput");
 const sizeSpan = document.querySelector("#sizeSpan");
+const canvas = document.querySelector("#canvas");
 
 // update span with the size of the new canvas
 function updateSpan() {
@@ -7,7 +8,36 @@ function updateSpan() {
     return ;
 }
 
+// delete every cell in the canvas
+function deleteCanvas() {
+    canvas.innerHTML = "";
+
+}
+
+function makeGrid(size=32) {
+    const width = canvas.offsetWidth; 
+
+    const itemsNum = size * size;
+    const itemWidth = width / size;
+
+
+    for (let i = 0; i < itemsNum; i++) {
+        let item = document.createElement("div");
+
+        item.style.width = itemWidth;
+        item.style.height = itemWidth;
+
+        item.classList.add("grid_item");
+
+        canvas.append(item);
+    }
+}
+
 // when range changes update the span size
 rangeInput.addEventListener("input", () => {
     updateSpan();
+    deleteCanvas();
+    makeGrid(rangeInput.value);
 });
+
+makeGrid();
